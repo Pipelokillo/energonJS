8d4ffd94f9635a60ffb5f2e0015e67ed7be095d1
Jose Barranco <jdcoding01@gmail.com>
2018-08-22 10:54:21 -0400

    Update energon.js

--- energon.js
+++ energon.js
@@ -38,7 +38,7 @@ const command = args.shift().toLowerCase();
   //Convert uppercase commands into lowercase
 
 let commandFile = require(`./modules/${command}.js`);
-   commandFile.run(AI, message, args);
+   commandFile.run(energon, message, args);
    //Read commands from modules folder
  
  });
