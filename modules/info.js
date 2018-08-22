206c6a490db7674af3ceb33824a34c28fc34e00a
Jose Barranco <jdcoding01@gmail.com>
2018-08-22 10:57:59 -0400

    Update info.js

--- modules/info.js
+++ modules/info.js
@@ -4,11 +4,12 @@ const data = require('../data.json');
 exports.run = (energon, message, args) => {
 
 const display = new app.RichEmbed()
-.setDescription("REV ScrimBot is a <:Fortnite:481831102802034689> bot for match info. Developed by Alphi#9839 <:staff:474743393658863626>")
+.setDescription("REV ScrimBot is a Fortnite bot for match info. Developed by Alphi#9839 <:staff:474743393658863626>")
 .setColor(`0x00ff00`)
 .setThumbnail("https://cdn.discordapp.com/emojis/481831653581258753.png?v=1")
 .setTimestamp()
 .addField("Bot owner", "REV Pipelokillo#2081")
 .setFooter("Requested by: " + message.author.username)
 message.channel.send(display);
+message.channel.send("<:Fortnite:481831102802034689>");
 }
