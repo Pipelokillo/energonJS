ab1d6d17dc9450040e09107d1cdd1b43590c53e1
Jose Barranco <jdcoding01@gmail.com>
2018-08-23 09:15:24 -0400

    Create display.js

---
+++ modules/display.js
@@ -0,0 +1,11 @@
+const app = require('discord.js');
+const data = require('../data.json');
+
+exports.run = (energon, message, args) => {
+const embed = new app.RichEmbed()
+.setDescription("Scrim list bot for Fortnite")
+.setThumbnail(energon.user.avatarURL
+.setTimestamp()
+.setFooter("Requested by: ", message.author.tag)
+const m = message.channel.send(embed);
+}
