0d1b0b031a27bbaa2542345389b9bbe4dccafeea
Jose Barranco <jdcoding01@gmail.com>
2018-08-23 09:26:57 -0400

    Update display.js

--- modules/display.js
+++ modules/display.js
@@ -4,8 +4,16 @@ const data = require('../data.json');
 exports.run = (energon, message, args) => {
 const embed = new app.RichEmbed()
 .setDescription("Scrim list bot for Fortnite")
-.setThumbnail(energon.user.avatarURL
+.setThumbnail(energon.user.avatarURL)
 .setTimestamp()
 .setFooter("Requested by: ", message.author.tag)
 const m = message.channel.send(embed);
-}
+energon.on('message', (message) => { 
+    if (message.channel.id === 'ID') {
+        //third step code
+    const stuff = message.content;
+    m.edit(stuff);
+    return;
+     }
+   }
+ }       
